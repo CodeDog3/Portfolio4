47e12d0487ce24525c39028aca80b074d84b3dfa
CodeDog3 <jonbernnan800@gmail.com>
2023-11-21 20:23:01 -0500

    full layout needs responsive design

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import ProjectSlider from "../_components/ProjectSlider";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center gap-5 md:flex-row h-screen bg-[#0C011A]">
+      <div className="flex flex-col gap-3">
+        <h1 className="text-[50px] text-white font-semibold">
+          My Work<span className="text-red-500">.</span>
+        </h1>
+        <p className="max-w-[400px] text-[16px] text-gray-200 md:text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <ProjectSlider />
+    </div>
+  );
+};
 
-export default Page
+export default Page;
